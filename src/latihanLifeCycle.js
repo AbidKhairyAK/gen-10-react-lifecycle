7f12838a1e2fbd41bc3f53eaff29f9f965463992
fanifadil <44403837+fanifadil@users.noreply.github.com>
2022-09-01 15:01:12 +0700

    Dari Fani

--- src/latihanLifeCycle.js
+++ src/latihanLifeCycle.js
@@ -2,44 +2,67 @@ import React from "react";
 
 const productFromApi = [
   {
-    nama : 'earphone',
-    harga : 20000
+    nama: "earphone",
+    harga: 20000,
   },
   {
-    nama : 'mouse',
-    harga : 50000
+    nama: "mouse",
+    harga: 50000,
   },
   {
-    nama : 'keyboard',
-    harga : 25000
+    nama: "keyboard",
+    harga: 25000,
   },
-]
+];
 
 export default class LatihanLifeCycle extends React.Component {
-  constructor () {
-    super() 
+  constructor() {
+    super();
     this.state = {
-      products :[]
-    } 
+      products: [],
+      carts: [
+        {
+          nama: "mouse",
+          harga: 50000,
+        },
+      ],
+    };
   }
 
   componentDidMount() {
-    this.setState({products : productFromApi})
+    this.setState({ products: productFromApi });
+  }
+
+  tambahKeranjang(produkYangDitambahkan) {
+    const keranjangSekarang = [...this.state.carts];
+    keranjangSekarang.push(produkYangDitambahkan);
+    this.setState({ carts: keranjangSekarang });
   }
 
   render() {
-    return <div>
-      Daftar produk 
-      <ul>
-        {this.state.products.map((barang)=>
-          <li>
-            {barang.nama} |
-            Rp. {barang.harga} |
-            <button> keranjang </button>
-          </li>
-        
-        )}
-      </ul>
-    </div>
+    return (
+      <div>
+        Daftar produk
+        <ul>
+          {this.state.products.map((barang) => (
+            <li>
+              {barang.nama} | Rp. {barang.harga} |
+              <button onClick={() => this.tambahKeranjang(barang)}>
+                {" "}
+                keranjang{" "}
+              </button>
+            </li>
+          ))}
+        </ul>
+        Daftar Keranjang
+        <ul>
+          {this.state.carts.map((cart) => (
+            <li>
+              {cart.nama} | Rp. {cart.harga}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
