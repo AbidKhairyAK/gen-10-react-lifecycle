4639e324c231a36607fbcf898cb847b1d7a0b4d4
Nurul <103403088+nurulfikriyatil@users.noreply.github.com>
2022-09-01 14:48:01 +0700

    ini dari nurul fikri

--- src/latihanLifeCycle.js
+++ src/latihanLifeCycle.js
@@ -1,7 +1,45 @@
 import React from "react";
 
+const productFromApi = [
+  {
+    nama : 'earphone',
+    harga : 20000
+  },
+  {
+    nama : 'mouse',
+    harga : 50000
+  },
+  {
+    nama : 'keyboard',
+    harga : 25000
+  },
+]
+
 export default class LatihanLifeCycle extends React.Component {
+  constructor () {
+    super() 
+    this.state = {
+      products :[]
+    } 
+  }
+
+  componentDidMount() {
+    this.setState({products : productFromApi})
+  }
+
   render() {
-    return <p>Hello World</p>;
+    return <div>
+      Daftar produk 
+      <ul>
+        {this.state.products.map((barang)=>
+          <li>
+            {barang.nama} |
+            Rp. {barang.harga} |
+            <button> keranjang </button>
+          </li>
+        
+        )}
+      </ul>
+    </div>
   }
 }
