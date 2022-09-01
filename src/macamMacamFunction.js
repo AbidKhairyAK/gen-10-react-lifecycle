8e1799387d282b26ec986d6ee2f7a84354296c22
AbidKhairyAK <abid.khairy.ak@gmail.com>
2022-09-01 10:49:25 +0700

    macam macam function

---
+++ src/macamMacamFunction.js
@@ -0,0 +1,14 @@
+
+function sebutNama1() {
+	alert('ahmad')
+}
+
+const sebutNama2 = function () {
+	alert('ahmad')
+}
+
+const sebutNama3 = () => {
+	return 'ahmad'
+}
+
+const sebutNama4 = () => 'ahmad'
